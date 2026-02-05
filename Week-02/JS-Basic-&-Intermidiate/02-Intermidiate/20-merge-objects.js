13f64332436217b3b2229524f8a3acf3b3632bab
CodecAnuj <anujtiwari9111@gmail.com>
2026-02-05 16:01:31 +0000

    Week-02-JS-Basic-&-Intermediate Done

---
+++ Week-02/JS-Basic-&-Intermidiate/02-Intermidiate/20-merge-objects.js
@@ -0,0 +1,12 @@
+/*
+  Question: Merge two objects (no sum, override second)
+  Input: { a: 10, b: 20 }, { a: 5, c: 15 }
+  Output: { a: 5, b: 20, c: 15 }
+*/
+
+const obj1 = { a: 10, b: 20 };
+const obj2 = { a: 5, c: 15 };
+
+const result = { ...obj1, ...obj2 };
+
+console.log(result);
