9710d251620d2e622cc522102a9c5c9c7a4445ab
CodecAnuj <anujtiwari9111@gmail.com>
2026-02-05 13:58:34 +0000

    Added JS-Intermediate Q13.Solution

--- Week-02/JS-Basic-&-Intermidiate/02-Intermidiate/13-most-repeated-word.js
+++ Week-02/JS-Basic-&-Intermidiate/02-Intermidiate/13-most-repeated-word.js
@@ -5,3 +5,28 @@
 */
 
 // solution
+const input = {
+  fruits: ["apple", "apple", "banana"],
+  drinks: ["apple", "tea"],
+};
+
+const allItems = Object.values(input).flat();
+console.log(allItems);
+
+const counts = allItems.reduce((acc, item) => {
+  acc[item] = (acc[item] || 0) + 1;
+  return acc;
+}, {});
+
+const winner = Object.entries(counts).reduce((champion, challenger) => {
+  const [champWord, champCount] = champion;
+  const [challengerWord, challengerCount] = challenger;
+
+  if (challengerCount > champCount) {
+    return challenger;
+  }
+
+  return champion;
+});
+
+console.log(winner[0]);
