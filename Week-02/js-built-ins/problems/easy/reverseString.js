d46000cae42af2df126699710f6157f6cdd0375c
CodecAnuj <anujtiwari9111@gmail.com>
2026-02-06 08:01:24 +0000

    Solved JS-Built-Ins reverseString

--- Week-02/js-built-ins/problems/easy/reverseString.js
+++ Week-02/js-built-ins/problems/easy/reverseString.js
@@ -19,7 +19,20 @@
 */
 
 function reverseString(str) {
-  // Your code here
+  let reversed = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversed += str[i];
+    // console.log(reversed);
+  }
+
+  return reversed;
 }
+
+// Better approach
+// function reverseString(str) {
+//   return str.split("").reverse().join("");
+// }
+
+// console.log(reverseString("Sumana"));
 
 module.exports = reverseString;
