55963f681b66847a08d2023011aa7ec97d51624e
CodecAnuj <anujtiwari9111@gmail.com>
2026-02-07 10:51:21 +0000

    solved 01-js expenditure-analysis

--- Week-02/01-js/easy/expenditure-analysis.js
+++ Week-02/01-js/easy/expenditure-analysis.js
@@ -9,7 +9,64 @@
 */
 
 function calculateTotalSpentByCategory(transactions) {
-    return [];
+  let categories = [];
+  let ans = [];
+
+  for (const { category, price } of transactions) {
+    if (categories.includes(category)) {
+      ans.forEach((ele) => {
+        if (ele.category == category) {
+          ele.totalSpent += price;
+        }
+      });
+    } else {
+      let obj = {};
+      categories.push(category);
+      obj.category = category;
+      obj.totalSpent = price;
+      ans.push(obj);
+    }
+  }
+  return ans;
 }
+const transactions = [
+  {
+    id: 1,
+    timestamp: 1656076800000,
+    price: 10,
+    category: "Food",
+    itemName: "Pizza",
+  },
+  {
+    id: 2,
+    timestamp: 1656259600000,
+    price: 20,
+    category: "Food",
+    itemName: "Burger",
+  },
+  {
+    id: 3,
+    timestamp: 1656019200000,
+    price: 15,
+    category: "Clothing",
+    itemName: "T-Shirt",
+  },
+  {
+    id: 4,
+    timestamp: 1656364800000,
+    price: 30,
+    category: "Electronics",
+    itemName: "Headphones",
+  },
+  {
+    id: 5,
+    timestamp: 1656105600000,
+    price: 25,
+    category: "Clothing",
+    itemName: "Jeans",
+  },
+];
+
+console.log(calculateTotalSpentByCategory(transactions));
 
 module.exports = calculateTotalSpentByCategory;
