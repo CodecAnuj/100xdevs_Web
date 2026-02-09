375a4a848704837ede3eafcebeeb58ffb399520e
CodecAnuj <anujtiwari9111@gmail.com>
2026-02-09 17:14:21 +0000

    Solve 03-Week-03 once and RejectAfter problem

--- Week-03-JS-Architecture/callbacks/easy/once.js
+++ Week-03-JS-Architecture/callbacks/easy/once.js
@@ -7,7 +7,37 @@
 // the callback with the same result (or error) from the first invocation.
 
 function once(fn) {
+  let catchedResult = null;
+  let finish = false;
+  let running = false;
+  const queue = [];
+  return function (...params) {
+    let callback = params.pop();
 
-}
+    if (finish) {
+      return callback(...catchedResult);
+    }
 
-module.exports = once;
+    if (running) {
+      queue.push(callback);
+      return;
+    }
+
+    running = true;
+
+    fn(...params, (err, ...args) => {
+      finish = true;
+
+      catchedResult = [err, ...args];
+
+      callback(err, ...args);
+
+      while (queue.length > 0) {
+        const waitingCb = queue.shift();
+        waitingCb(err, ...args);
+      }
+    });
+  };
+}1
+
+module.exports = once; /*  */
