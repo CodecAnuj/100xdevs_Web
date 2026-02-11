a0f575149367d9120ced09824625fd208efbc1a6
CodecAnuj <anujtiwari9111@gmail.com>
2026-02-11 18:33:34 +0000

    Updated Week-03 once.js

--- Week-03-JS-Architecture/callbacks/easy/once.js
+++ Week-03-JS-Architecture/callbacks/easy/once.js
@@ -7,37 +7,79 @@
 // the callback with the same result (or error) from the first invocation.
 
 function once(fn) {
-  let catchedResult = null;
+  let result = null;
   let finish = false;
   let running = false;
-  const queue = [];
+  let queue = [];
+
   return function (...params) {
     let callback = params.pop();
 
     if (finish) {
-      return callback(...catchedResult);
+      return callback(...result);
     }
+
+    queue.push(callback);
 
     if (running) {
       queue.push(callback);
-      return;
+      return
     }
 
     running = true;
 
-    fn(...params, (err, ...args) => {
+    fn(...params, (...args) => {
+      result = args;
       finish = true;
 
-      catchedResult = [err, ...args];
-
-      callback(err, ...args);
-
       while (queue.length > 0) {
-        const waitingCb = queue.shift();
-        waitingCb(err, ...args);
+        let cb = queue.shift();
+        cb(...result)
       }
-    });
+    })
   };
 }
+
+
+// explanation
+// function once(fn) {
+//   let result = null; // To store [err, data1, data2...]
+//   let finish = false; // Flag: Is calculation done?
+//   let running = false; // Flag: Is function currently running?
+//   let queue = []; // Queue for all callers (including the first one)
+
+//   return function (...params) {
+//     let callback = params.pop();
+
+//     if (finish) {
+//       // If finished, return cached result immediately
+//       return callback(...result);
+//     }
+
+//     // Push everyone to queue.
+//     // Logic: Even the first caller waits in queue until fn completes.
+//     queue.push(callback);
+
+//     // If 'fn' is already running, we stop here.
+//     // The queue will be processed when 'fn' finishes.
+//     if (running) {
+//       return;
+//     }
+
+//     running = true; // Mark as running
+
+//     fn(...params, (...args) => {
+//       // Save all arguments (error + data) to support any callback signature
+//       result = args;
+//       finish = true;
+
+//       // Process the queue and send result to all waiting callbacks
+//       while (queue.length) {
+//         let cb = queue.shift();
+//         cb(...result);
+//       }
+//     });
+//   };
+// }
 
 module.exports = once; /*  */
