db57b121ca8fd624d5da9b499a2178af16a1b08a
CodecAnuj <anujtiwari9111@gmail.com>
2026-02-27 14:54:41 +0000

    solved runWithDependencies

--- Week-03-04-Architecture-&-Async-JS/callbacks/medium/runWithDependencies.js
+++ Week-03-04-Architecture-&-Async-JS/callbacks/medium/runWithDependencies.js
@@ -11,6 +11,51 @@
 // Invoke finalCallback after all tasks have completed, or with an error
 // if any task fails.
 
-function runWithDependencies(tasks, finalCallback) {}
+function runWithDependencies(tasks, finalCallback) {
+  const results = {};
+  const running = new Set();
+  let hasError = false;
+
+  function checkManager() {
+    if (hasError) return;
+
+    tasks.forEach((task) => {
+      if (results[task.id] || running.has(task.id)) {
+        return;
+      }
+
+      const canStart = task.deps.every((dep) => {
+        return results[dep] !== undefined;
+      });
+
+      if (canStart) {
+        running.add(task.id);
+
+        task.run((err, result) => {
+          if (hasError) return;
+
+          if (err) {
+            hasError = true;
+            return finalCallback(err);
+          }
+
+          // save result
+          results[task.id] = result;
+
+          // remove from running
+          running.delete(task.id);
+
+          if (Object.keys(results).length === tasks.length) {
+            return finalCallback(null, results);
+          } else {
+            checkManager();
+          }
+        });
+      }
+    });
+  }
+
+  checkManager();
+}
 
 module.exports = runWithDependencies;
