8ba69442f444586f944bc395cfa6846b657c2a57
CodecAnuj <anujtiwari9111@gmail.com>
2026-02-28 10:46:37 +0000

    Solved fifoMutex

--- Week-03-04-Architecture-&-Async-JS/callbacks/hard/fifoMutex.js
+++ Week-03-04-Architecture-&-Async-JS/callbacks/hard/fifoMutex.js
@@ -12,12 +12,57 @@
 // - Run immediately if free.
 // - Queue when locked (FIFO).
 // - Auto-release on task completion.
+
 class Mutex {
-  constructor() {}
+  constructor() {
+    this.isLocked = false; // flag to track running task
+    this.queue = []; // FIFO Queue -> item = { task, onComplete }
+  }
 
-  lock(task, onComplete) {}
+  lock(task, onComplete) {
+    // If no task is running, start immediately
+    if (!this.isLocked) {
+      this.isLocked = true;
+      this._run(task, onComplete);
+    } else {
+      // If already locked, push the task into queue
+      this.queue.push({ task, onComplete });
+    }
+  }
 
-  _release() {}
+  _run(task, onComplete) {
+    try {
+      // Execute the task
+      task((...args) => {
+        // Always release lock BEFORE calling user callback
+        // This ensures queue continues even if callback throws
+        this._release();
+
+        // Call user-provided completion handler
+        if (onComplete) onComplete(...args);
+      });
+      
+    } catch (err) {
+      // If task throws synchronously,
+      // still release lock to avoid deadlock
+      this._release();
+
+      if (onComplete) onComplete(err);
+    }
+  }
+
+  _release() {
+    // If no one is waiting, unlock
+    if (this.queue.length === 0) {
+      this.isLocked = false;
+    } else {
+      // Otherwise, take next task from queue (FIFO)
+      const next = this.queue.shift();
+
+      // Start next task immediately
+      this._run(next.task, next.onComplete);
+    }
+  }
 }
 
 module.exports = Mutex;
