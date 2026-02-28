ff5be86ddac4fb87a99a0a3c0722c1be78385e5f
CodecAnuj <anujtiwari9111@gmail.com>
2026-02-28 17:49:34 +0000

    Solved mapLimit

--- Week-03-04-Architecture-&-Async-JS/callbacks/hard/mapLimit.js
+++ Week-03-04-Architecture-&-Async-JS/callbacks/hard/mapLimit.js
@@ -10,7 +10,49 @@
 // - Accept an array of tasks and a concurrency limit.
 // - Run at most `limit` tasks in parallel until all are completed.
 // - Return results in the original task order via onAllFinished.
+//
+// Once you've implemented the logic, test your code by running
+//   - `npm run mapLimit`
 
-function mapLimit(tasks, limit, onAllFinished) {}
+function mapLimit(tasks, limit, onAllFinished) {
+  let results = new Array(tasks.length);
+  let nextIndex = 0;
+  let runningCount = 0;
+  let completedCount = 0;
+  let hasError = false;
+
+  function startNext() {
+    if (hasError) return;
+    if (runningCount >= limit) return;
+    if (nextIndex >= tasks.length) return;
+
+    let currentindex = nextIndex;
+    nextIndex++;
+    runningCount++;
+
+    tasks[currentindex]((err, res) => {
+      if (hasError) return;
+
+      if (err) {
+        hasError = true;
+        onAllFinished(err);
+        return;
+      }
+      results[currentindex] = res;
+      runningCount--;
+      completedCount++;
+
+      if (completedCount === tasks.length) {
+        onAllFinished(null, results);
+      } else {
+        startNext();
+      }
+    });
+  }
+
+  while (runningCount < limit && nextIndex < tasks.length) {
+    startNext();
+  }
+}
 
 module.exports = mapLimit;
