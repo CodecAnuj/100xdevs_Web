a25a5c48cab179d274a28a4ff207151edb6ebfbd
CodecAnuj <anujtiwari9111@gmail.com>
2026-03-01 17:00:55 +0000

    Solved Hard Schedular assignment

--- Week-03-04-Architecture-&-Async-JS/callbacks/hard/Scheduler.js
+++ Week-03-04-Architecture-&-Async-JS/callbacks/hard/Scheduler.js
@@ -9,13 +9,49 @@
 //
 // True preemption is not possible in JavaScript, so tasks must cooperate
 // by yielding execution voluntarily.
+//
+// Once you've implemented the logic, test your code by running
+//   - `npm run Scheduler`
 
 class Scheduler {
-  constructor() {}
+  constructor() {
+    this.queue = [];
+    this.running = false;
+    this.onAllFinished = null;
+  }
 
-  schedule(task, priority = 0) {}
+  schedule(task, priority = 0) {
+    this.queue.push({ task, priority });
+    this.queue.sort((a, b) => b.priority - a.priority);
+  }
 
-  run(onAllFinished) {}
+  run(onAllFinished) {
+    this.onAllFinished = onAllFinished;
+
+    const runNext = () => {
+      if (this.running) return;
+
+      if (this.queue.length === 0) {
+        if (this.onAllFinished) this.onAllFinished(null);
+        return;
+      }
+
+      const item = this.queue.shift();
+      this.running = true;
+
+      item.task((err) => {
+        this.running = false;
+
+        if (err) {
+          if (this.onAllFinished) this.onAllFinished(err);
+          return;
+        }
+        runNext();
+      });
+    };
+
+    runNext();
+  }
 }
 
 module.exports = Scheduler;
