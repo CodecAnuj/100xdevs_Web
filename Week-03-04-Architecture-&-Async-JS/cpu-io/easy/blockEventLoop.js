ac4f6cb5d07cd2fa116be7e4457767b7c6eaf939
CodecAnuj <anujtiwari9111@gmail.com>
2026-03-02 07:31:23 +0000

    Solved blockEventLoop

--- Week-03-04-Architecture-&-Async-JS/cpu-io/easy/blockEventLoop.js
+++ Week-03-04-Architecture-&-Async-JS/cpu-io/easy/blockEventLoop.js
@@ -1,10 +1,3 @@
-
-
-// bhai mujhe asise sikhao pahile tum mujhe sabhi neccessary concepts samjhao detail me then mujhe kudh code karne do usko review karo or mujhe batao maine kya sahi kiya kya galat kya correct approach kya hogi optimzed kaise karenge aise taki meri real me learning ho
-
-// This is my next assignment question. I attempted this assignment but was not able to understand it, and I want to know if there are some earlier ones I should do before attempting this. If not, then how should I approach these questions?.
-// Problem Description – Block Event Loop
-//
 // In Node.js, long-running synchronous operations block the event loop,
 // preventing other tasks (like timers or I/O) from executing.
 //
