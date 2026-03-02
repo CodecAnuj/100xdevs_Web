b2a521bb7bd1d44ce29b1366be25d08cd94e7872
CodecAnuj <anujtiwari9111@gmail.com>
2026-03-02 12:59:59 +0000

    Solved eventLoopRace

--- Week-03-04-Architecture-&-Async-JS/cpu-io/easy/eventLoopRace.js
+++ Week-03-04-Architecture-&-Async-JS/cpu-io/easy/eventLoopRace.js
@@ -6,8 +6,23 @@
 // Your task is to understand and predict the order in which the logs
 // are printed to the console.
 //
+// "1: Synchronous",
+// "4: Synchronous",
+// "3: Microtask (Promise)",
+// "2: Macrotask (I/O)",
 
+function eventLoopRace() {
+  console.log("1: Synchronous");
 
-function eventLoopRace() {}
+  setTimeout(() => {
+    console.log("2: Macrotask (I/O)");
+  }, 0);
+
+  Promise.resolve().then(() => {
+    console.log("3: Microtask (Promise)");
+  });
+
+  console.log("4: Synchronous");
+}
 
 module.exports = eventLoopRace;
