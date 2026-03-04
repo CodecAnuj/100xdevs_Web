e0c5d1c52d7157dbfcb4fc1c6761e124f02321e3
CodecAnuj <anujtiwari9111@gmail.com>
2026-03-04 18:05:58 +0000

    Solved measureTime

--- Week-03-04-Architecture-&-Async-JS/cpu-io/easy/measureTime.js
+++ Week-03-04-Architecture-&-Async-JS/cpu-io/easy/measureTime.js
@@ -8,6 +8,15 @@
 // 2. The function should handle errors (if fn throws, you should still catch the time or rethrow)
 // 3. Use performance.now() or Date.now() for timing.
 
-async function measureTime(fn) {}
+async function measureTime(fn) {
+  try {
+    const start = performance.now();
+    await fn();
+    const end = performance.now() - start;
+    return end;
+  } catch (error) {
+    throw error;
+  }
+}
 
 module.exports = measureTime;
