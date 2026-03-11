684f3b20af55603e28d97e9fb9a357cdfe8f97f0
CodecAnuj <anujtiwari9111@gmail.com>
2026-03-11 10:12:04 +0000

    Solved runSequential

--- Week-03-04-Architecture-&-Async-JS/cpu-io/easy/runSequential.js
+++ Week-03-04-Architecture-&-Async-JS/cpu-io/easy/runSequential.js
@@ -1,8 +1,16 @@
 // Problem Description – Sequential Execution of Async Functions
 
-// You are given an array of asynchronous functions. Your task is to execute them one by one, ensuring that each function starts only after the previous one has completed. 
+// You are given an array of asynchronous functions. Your task is to execute them one by one, ensuring that each function starts only after the previous one has completed.
 // The final result should be an array of resolved values in the same order.
-async function runSequential(functions) {}
+async function runSequential(functions) {
+  const result = [];
+
+  for (const fn of functions) {
+    const value = await fn();
+    result.push(value);
+  }
+
+  return result;
+}
 
 module.exports = runSequential;
-
