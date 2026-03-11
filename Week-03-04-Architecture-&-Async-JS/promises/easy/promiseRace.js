9139672d0ff10b59fa6745ecb2da6f13eb711798
CodecAnuj <anujtiwari9111@gmail.com>
2026-03-11 14:42:52 +0000

    Solved promiseRace

--- Week-03-04-Architecture-&-Async-JS/promises/easy/promiseRace.js
+++ Week-03-04-Architecture-&-Async-JS/promises/easy/promiseRace.js
@@ -1,10 +1,26 @@
 // Problem Description – Custom Implementation of Promise.race
 
-// You are required to implement your own version of Promise.race without using the built-in method. 
-// The function should accept an iterable of values that may include Promises or plain values. 
-// It must settle as soon as the first input settles, resolving or rejecting accordingly. 
+// You are required to implement your own version of Promise.race without using the built-in method.
+// The function should accept an iterable of values that may include Promises or plain values.
+// It must settle as soon as the first input settles, resolving or rejecting accordingly.
 // Using Promise.resolve ensures non-promise values are handled correctly.
-function promiseRace(promises) {}
+
+function promiseRace(promises) {
+  if (!Array.isArray(promises)) {
+    return reject(new TypeError("Input must be an array"));
+  }
+  
+  return new Promise((resolve, reject) => {
+    for (let i = 0; i < promises.length; i++) {
+      Promise.resolve(promises[i])
+        .then((result) => {
+          resolve(result);
+        })
+        .catch((err) => {
+          reject(err);
+        });
+    }
+  });
+}
 
 module.exports = promiseRace;
-
